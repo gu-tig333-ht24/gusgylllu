87aba7c2d91a8ec9be341e30a2ef214aa1997c9a
LucasGyllen <lucas.gyllensvaan@gmail.com>
2024-10-03 08:22:18 +0200

    finished with part 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,129 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TIG333 TODO',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Lucas App är cool'),
+      home: const TodoList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TodoListState createState() => _TodoListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter += 1;
-    });
-  }
+class _TodoListState extends State<TodoList> {
+  final TextEditingController _controller = TextEditingController();
+  final List<Map<String, dynamic>> _todoItems = [
+    {'title': 'Write a book', 'done': false},
+    {'title': 'Do homework', 'done': false},
+    {'title': 'Tidy room', 'done': true},
+    {'title': 'Watch TV', 'done': false},
+    {'title': 'Nap', 'done': false},
+    {'title': 'Shop groceries', 'done': false},
+    {'title': 'Have fun', 'done': false},
+    {'title': 'Meditate', 'done': false},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('TIG333 TODO', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.grey[300],
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (String result) {},
+            itemBuilder: (BuildContext context) => [
+              const PopupMenuItem(
+                value: 'all',
+                child: Text('All'),
+              ),
+              const PopupMenuItem(
+                value: 'done',
+                child: Text('Done'),
+              ),
+              const PopupMenuItem(
+                value: 'undone',
+                child: Text('Undone'),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: 'What are you going to do?',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: null,//will call a function to add todo item to list.
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todoItems.length,
+              itemBuilder: (context, index) {
+                return _buildTodoItem(_todoItems[index]['title'], _todoItems[index]['done'], index);
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: null,//will call a function to add todo item to list.
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Colors.grey[300],
+        elevation: 6.0,
+      ),
+    );
+  }
+
+  Widget _buildTodoItem(String title, bool done, int index) {
+    return ListTile(
+      leading: Transform.scale(
+        scale: 1.3,
+        child: Checkbox(
+          value: done,
+          onChanged: (bool? value) {
+            //will later call a method for the ability to toggle completed tasks on the todo list 
+          },
+        ),
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          decoration: done ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: IconButton(
+        icon: const Icon(Icons.close),
+        onPressed: () {
+          //will call a function to remove todo item from list.
+        },
+      ),
     );
   }
 }
